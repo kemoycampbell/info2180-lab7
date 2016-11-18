d3fef2e2e28d838681c584d3a21b72c805d13bf8
Romario Maxwell <romario.maxwell@gmail.com>
2016-11-17 23:45:41 -0500

    Modify style code
    I misinterpreted the lab instructions thinking that we could not modify the index.html file for some reason and unfortunately used Javascript to do all the styling... I have removed that code and have linked and used external resources.

--- world.js
+++ world.js
@@ -10,59 +10,7 @@ window.addEventListener("load", function() {
     // DOM
     var countryField = doc.getElementById("country");
     var lookupBtn = doc.getElementById("lookup");
-    var controls = doc.getElementById("controls");
     var result = doc.getElementById("result");
-    var h1Ele = doc.getElementsByTagName("h1")[0];
-    var labelEle = doc.getElementsByTagName("label")[0];
-    var headEle = doc.getElementsByTagName("head")[0];
-    var bodyEle = doc.getElementsByTagName("body")[0];
-    
-    var scriptEle;
-    var linkEle;
-    var divEle;
-    
-    linkEle = doc.createElement("link");
-    linkEle.setAttribute("href", "https://fonts.googleapis.com/css?family=Roboto");
-    linkEle.setAttribute("rel", "stylesheet");
-    headEle.appendChild(linkEle);
-    
-    linkEle = doc.createElement("link");
-    linkEle.setAttribute("href", "https://fonts.googleapis.com/icon?family=Material+Icons");
-    linkEle.setAttribute("rel", "stylesheet");
-    headEle.appendChild(linkEle);
-    
-    linkEle = doc.createElement("link");
-    linkEle.setAttribute("href", "https://code.getmdl.io/1.2.1/material.indigo-pink.min.css");
-    linkEle.setAttribute("rel", "stylesheet");
-    headEle.appendChild(linkEle);
-    
-    scriptEle = doc.createElement("script");
-    scriptEle.setAttribute("src", "https://code.getmdl.io/1.2.1/material.min.js");
-    headEle.appendChild(scriptEle);
-    
-    controls.appendChild(labelEle);
-    controls.appendChild(doc.createElement("br"));
-    controls.appendChild(countryField);
-    
-    lookupBtn.style.marginLeft = "10px";
-    
-    controls.appendChild(lookupBtn);
-    
-    divEle = doc.createElement("div");
-    divEle.style.margin = "auto";
-    divEle.style.width = "50%";
-    divEle.style.backgroundColor = "#E0E0E0";
-    divEle.style.padding = "25px";
-    divEle.style.marginTop = "25px";
-    divEle.style.borderRadius = "10px";
-
-    divEle.appendChild(h1Ele);
-    divEle.appendChild(controls);
-    divEle.appendChild(result);
-    
-    bodyEle.appendChild(divEle);
-    
-    lookupBtn.setAttribute("class", "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
     
     lookupBtn.addEventListener("click", function() {
         country = countryField.value;
